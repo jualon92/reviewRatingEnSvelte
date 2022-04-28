cdfe5e9abbfef2bc5e25d5817a9bae146614e822
jualon92 <londrajuan@gmail.com>
2022-04-28 18:46:10 -0300

    svelte compatibilidad 2

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path');
 const port = process.env.PORT || 5000; 
 
 
-app.use(express.static('public')); //que utilize public
+//app.use(express.static('public')); //que utilize public
+app.use('/build', express.static('public/build'))
+
 
 app.get('*', (req, res) => {  //index html entrada
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
