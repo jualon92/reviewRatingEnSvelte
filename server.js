d8271b4e94c76e57ae4cbd5d19679533aa19cf75
jualon92 <londrajuan@gmail.com>
2022-04-28 19:07:06 -0300

    cambios heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 5000; 
-
+ 
+const PORT = process.env.PORT || 3000;
 
 //app.use(express.static('public')); //que utilize public
 app.use('/build', express.static('public/build'))
@@ -11,10 +11,8 @@ app.use('/build', express.static('public/build'))
 
 app.get('*', (req, res) => {  //index html entrada
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  });
-
-  app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-    console.log("Server is running.");
-  });
-
-   
+});
+ 
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
+});
