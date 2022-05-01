36658b158b5e8e4f4af3ee7334e1c18962d4d02c
jualon92 <londrajuan@gmail.com>
2022-05-01 11:08:39 -0300

    parseInt a id review producia NaN

--- src/stores.js
+++ src/stores.js
@@ -12,7 +12,7 @@ const procesarData = (listaJSON) => {
     let lista = []
     listaJSON.forEach(eleDB => {
         const nuevoEle = {
-            "id": parseInt(eleDB.id),
+            "id": eleDB.id,
             "rating": Number(eleDB.rating),
             "text": eleDB.text
         }
@@ -26,6 +26,8 @@ async function getData() {
     const response = await fetch(apiURL);
     const rta = await response.json()
     console.log('Response:', rta);
+
+
     const data = procesarData(rta)
     console.log("Data procesada", data)
     FeedbackStore.set(data)
